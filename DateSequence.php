48f0e3c1ad96b7eed0364bcf58caae20089a2885
Takeru-chan <takeru.katayama@gmail.com>
2018-04-17 20:15:37 +0900

    DateSequence class is added.

---
+++ DateSequence.php
@@ -0,0 +1,91 @@
+<?php
+/*
+# DateSequence.php ver.0.1.0  2018.4.17  (c)Takeru.
+#
+# 指定年月のDatetimeクラスの拡張日付セット配列を生成
+# 日付セットは指定年月の前後を含むことがあり、全部で42日（６週分）の配列
+# getDate($index)で指定インデックスのDatetimeXクラスを返す
+#   0~41以外の値を入力すると落ちる件は対策検討中
+# getDate('curr')は指定年月（実行日ではない）初日のDatetimeXクラスを返す
+# getDate('prev')は指定年月前月の初日のDatetimeXクラスを返す
+# getDate('next')は指定年月翌月の初日のDatetimeXクラスを返す
+#   DatetimeXクラスにはstatus[]配列が追加されている
+#   status['range'] : 当該月の日付 = 0, 前月の日付 = -1, 翌月の日付 = 1
+#   status['today'] : 実行日当日 = 1, それ以外 = 0
+#   status['holiday'] : 休日 = 1, 平日 = 0（現在メソッド未実装）
+#   これらの値はDatetimeXクラスに対してgetStatus()メソッドで取得できる
+#
+#        Copyright (c) 2018 Takeru.
+#        Release under the MIT license
+#        http://opensource.org/licenses/MIT
+#
+date_default_timezone_set('Asia/Tokyo');
+$now = new Datetime();
+$year = $now->format('Y');
+$month = $now->format('n');
+$diff = $now->format('j') + (new Datetime($year.'-'.$month.'-1'))->format('w') - 1;
+$dt = new DateSequence($year,$month);
+var_dump($dt->getDate($diff)->getStatus());
+var_dump($dt->getDate('curr')->format('Ym'));
+var_dump($dt->getDate('prev')->format('Ym'));
+var_dump($dt->getDate('next')->format('Ym'));
+var_dump($dt->getDate(0)->getStatus());
+*/
+
+class Datetimex extends Datetime {
+  private $status;
+  function setRange($status){
+    $this->status['range'] = $status;
+  }
+  function setToday($status){
+    $this->status['today'] = $status;
+  }
+  function setHoliday($status){
+    $this->status['holiday'] = $status;
+  }
+  function getStatus() {
+    return $this->status;
+  }
+}
+class DateSequence {
+  private $dateTime;
+  private $dateSequence;
+  function __construct($year, $month) {
+    $this->dateTime['now'] = (new Datetimex())->setTime(0,0);
+    $year = ($year > 1900 && $year < 10000) ? $year : $now->format('Y');
+    $month = ($month > 0 && $month < 13) ? $month : $now->format('n');
+    $this->dateTime['curr'] = new Datetimex($year."-".$month."-1");
+    $date = (clone $this->dateTime['curr'])->modify('-1 days');
+    $this->dateTime['prev'] = new Datetimex($date->format('Y')."-".$date->format('n')."-1");
+    $date = $date->modify('32 days');
+    $this->dateTime['next'] = new Datetimex($date->format('Y')."-".$date->format('n')."-1");
+
+    $date = clone $this->dateTime['curr'];
+    $this->dateSequence[] = $date->modify('-'.(int)$date->format('w').' days');
+    for ($i=1;$i<42;$i++) {
+      $this->dateSequence[$i] = (clone $this->dateSequence[$i - 1])->modify('1 days');
+    }
+    for ($i=0;$i<42;$i++) {
+      $this->dateSequence[$i]->setRange((int)$this->dateSequence[$i]->format('n') - (int)$this->dateTime['curr']->format('n'));
+      if ($this->dateSequence[$i] == $this->dateTime['now']) {
+        $this->dateSequence[$i]->setToday(1);
+      } else {
+        $this->dateSequence[$i]->setToday(0);
+      }
+/*
+      if ($this->dateSequence[$i]->format('w') == 1) {
+        $this->dateSequence[$i]->setHoliday(1);
+      }
+*/
+    }
+  }
+  function getDate($index) {
+    if (is_int($index)) {
+      return $this->dateSequence[$index];
+    } elseif ($index == 'curr' || $index == 'prev' || $index == 'next') {
+      return $this->dateTime[$index];
+    } else {
+      return null;
+    }
+  }
+}
--- README.md
+++ README.md
@@ -8,6 +8,25 @@
 - getLines() - 空行および行頭#行を除いた１行１データの配列を返す  
 - getAllLines() - 空行等を含む全ての行を１行１データの配列で返す  
 
+## DateSequence.php
+指定年月のDatetimeクラス拡張日付セットの配列を返すクラス。  
+
+一ヶ月分のカレンダーを生成する場合等、当該年月を含む42日（６週）分の日付情報を取得するのに使う。
+DatetimeXクラスとともに使用して、前月/翌月や当日か否か、休日か否かの付加情報を得る。  
+
+- getDate($index) - 指定インデックスのDatetimeXクラスを返す（0<=$index<=41）  
+- getDate('curr') - クラス生成指定年月（実行日ではない）初日のDatetimeXクラスを返す  
+- getDate('prev') - クラス生成指定年月前月の初日のDatetimeXクラスを返す  
+- getDate('next') - クラス生成指定年月翌月の初日のDatetimeXクラスを返す  
+
+## DatetimeX
+DateSequenceクラス用のDatetime拡張クラス。  
+
+- getStaus() - DatetimeXクラスの付加情報を返す  
+  status['range'] : 指定年月の日付 = 0, 指定年月前月の日付 = -1, 指定年月翌月の日付 = 1  
+  status['today'] : 実行日当日 = 1, それ以外 = 0  
+  status['holiday] : 休日 = 1, 平日 = 0（メソッド未実装）  
+
 ## License
 This script has released under the MIT license.  
 [http://opensource.org/licenses/MIT](http://opensource.org/licenses/MIT)
